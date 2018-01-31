6cd01cd95857d0f20de55de8ffac2822b5131f4d
stacype <stacy.peairs@live.com>
2018-01-31 13:38:24 -0800

    moved js out of html to it's home

--- app.js
+++ app.js
@@ -1,47 +1,27 @@
 
         
-            alert('Hello There');
-            const name = prompt('What is your name?');
-            alert('Hi there ' + name);
-            console.log('name is ' + typeof name);
-            console.log('name is: ' + name);
-            
-            const icecream = prompt('Do you like ice cream?');
-            alert ('likes ice cream');
-            console.log('likesIcecream is ' + typeof likesIcecream);
-            console.log('likes Icecream: ' + likesIcecream);
-            
-            function likesIcecream(yes) {
-                 if (y > 0) {
-                     return "positive";
-                } else {
-                return "NOT positive";
-                }
-            }
-
-            console.log(likesIcecream(0));
-            // expected output: "NOT positive"
-
-            const funny = prompt('Are you funny?');
-            alert ('are funny');
-            console.log('funny is ' + typeof funny);
-            console.log('funny: ' + funny);
-            if (funny === 'yes' === 'y') {
-                return "positive";
-           } else if {'funny'  :'no') 
-           return : "NOT positive";
-           }
-       }
-
-            const sunny = prompt('Is it sunny today?');
-            alert ('is sunny');
-            console.log('is sunny ' + typeof sunny);
-            console.log('is sunny: ' + sunny);
-
-            const drive = prompt('Did you drive today?');
-            alert ('did drive');
-            console.log('did drive ' + typeof drive);
-            console.log('is drive: ' + ddrive);
+             alert('Hello There');
+             const name = prompt('What is your name?');
+             alert('Hi there ' + name);
+             console.log('name is ' + name);
+             console.log('name is: ' + name);
+             
+             const likesIcecream = prompt('Do you like ice cream?');
+             alert ('ice cream ' + likesIcecream);
+             console.log('likes Ice cream  ' + typeof string);
+             console.log('likes Ice cream: ' + likesIcecream);
+ 
+             const funny = prompt('Are you funny?');
+             alert ('is funny ' + funny);
+             console.log('funny  ' + typeof string);
+             console.log('funny: ' + funny);
+ 
+             const sunny = prompt('Is it sunny today?');
+             alert ('is sunny ' + sunny);
+             console.log('is sunny ' + typeof string);
+             console.log('is sunny: ' + sunny);
+ 
+         
 
         
     
