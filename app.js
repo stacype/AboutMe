d4337e583c7035d9508b7255308acecee3fcee8a
stacype <stacy.peairs@live.com>
2018-01-31 15:09:14 -0800

    get's reply to ice cream like and adds name to it

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 
-        
-             alert('Hello There');
-             const name = prompt('What is your name?');
-             alert('Hi there ' + name);
-             console.log('name is ' + name);
-             console.log('name is: ' + name);
-             
-             const likesIcecream = prompt('Do you like ice cream?');
-             alert ('ice cream ' + likesIcecream);
-             console.log('likes Ice cream  ' + typeof string);
-             console.log('likes Ice cream: ' + likesIcecream);
- 
-             const funny = prompt('Are you funny?');
-             alert ('is funny ' + funny);
-             console.log('funny  ' + typeof string);
-             console.log('funny: ' + funny);
- 
-             const sunny = prompt('Is it sunny today?');
-             alert ('is sunny ' + sunny);
-             console.log('is sunny ' + typeof string);
-             console.log('is sunny: ' + sunny);
- 
-         
 
-        
-    
+
+'use strict';
+alert('Hello There');
+const name = prompt('What is your name?');
+alert('Hi there ' + name);
+console.log('name is ' + name);
+console.log('name is: ' + name);
+
+const likesIcecream = prompt('Do you like ice cream?');
+alert (name + ' likes Ice cream ' + likesIcecream);
+console.log('likes Ice cream  ' + typeof string);
+console.log('likes Ice cream: ' + likesIcecream);
+
+const funny = prompt('Are you funny?');
+alert ('is funny ' + funny);
+console.log('funny  ' + typeof string);
+console.log('funny: ' + funny);
+
+const sunny = prompt('Is it sunny today?');
+alert ('is sunny ' + sunny);
+console.log('is sunny ' + typeof string);
+console.log('is sunny: ' + sunny);
+
+
+
+
