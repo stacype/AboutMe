267ae2d3758c128eadbc07a3e1ed4548708e47ae
stacype <35848691+stacype@users.noreply.github.com>
2018-01-30 20:42:27 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+
+        
+            alert('Hello There');
+            const name = prompt('What is your name?');
+            alert('Hi there ' + name);
+            console.log('name is ' + typeof name);
+            console.log('name is: ' + name);
+            
+            const icecream = prompt('Do you like ice cream?');
+            alert ('likes ice cream');
+            console.log('likesIcecream is ' + typeof likesIcecream);
+            console.log('likes Icecream: ' + likesIcecream);
+            
+            function likesIcecream(yes) {
+                 if (y > 0) {
+                     return "positive";
+                } else {
+                return "NOT positive";
+                }
+            }
+
+            console.log(likesIcecream(0));
+            // expected output: "NOT positive"
+
+            const funny = prompt('Are you funny?');
+            alert ('are funny');
+            console.log('funny is ' + typeof funny);
+            console.log('funny: ' + funny);
+
+            const sunny = prompt('Is it sunny today?');
+            alert ('is sunny');
+            console.log('is sunny ' + typeof sunny);
+            console.log('is sunny: ' + sunny);
+
+            const drive = prompt('Did you drive today?');
+            alert ('did drive');
+            console.log('did drive ' + typeof drive);
+            console.log('is drive: ' + ddrive);
+
+        
+    
